5f4c101bbdb05dd2221e1a9655c5b01e9afa16f3
DRV <davizjo@gmail.com>
2017-04-15 20:05:45 +0200

    Added door rotation

--- Source/MyProject/OpenDoor.cpp
+++ Source/MyProject/OpenDoor.cpp
@@ -1,11 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright David Ruiz Vallejo 2017
 
 #include "MyProject.h"
-#include "PositionReporter.h"
+#include "OpenDoor.h"
 
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,18 +16,18 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
-	FString objectPos = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *GetOwner()->GetName(), *objectPos);
+	FRotator doorRotation = FRotator(0.0f, 90.0f, 0.0f);
+	GetOwner()->SetActorRotation(doorRotation);
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
